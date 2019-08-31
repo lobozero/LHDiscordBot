ae0fabbb8db381128a0f2da754d16237c4b4b1ed
lobozero <lobo_zero@yahoo.com.br>
2019-08-31 13:04:56 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,4 +26,4 @@ client.on('message', message => {
 
 // THIS  MUST  BE  THIS  WAY
 
-client.login(process.env.NjE3Mzc0MDM3MDAxMjQwNTg1.XWqMqA.xy3HliNwRkn35gbFLeSSZMn6FQM);//BOT_TOKEN is the Client Secret
+client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
